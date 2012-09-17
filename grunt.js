f12315d3dbf9c93638551a97e8204eb1ac4f6f92
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-09-17 11:02:03 +0200

    Fix grunt crashing on watch

--- grunt.js
+++ grunt.js
@@ -53,7 +53,7 @@ module.exports = function (grunt) {
       }
     },
     watch:{
-      files:['<config:lint.files>', '<config.src.html>'],
+      files:['<config:src.js>', '<config:test.js>', '<config:src.less>', 'src/index.html'], //need to have a path to index.html, otherwise watch won't pick it up
       tasks:'build'
     },
     jshint:{
