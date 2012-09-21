9b3aefc82dab8bf9e22357fa84d7f6324faea74b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-09-21 18:41:50 +0200

    Add validation for password / password repeat

--- src/modules/admin/admin-users.js
+++ src/modules/admin/admin-users.js
@@ -67,3 +67,36 @@ angular.module('admin-users').directive('uniqueEmail', ["Users", function (Users
     }
   };
 }]);
+
+angular.module('admin-users').directive('validateEquals', function() {
+
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+
+      function validateEqual(myValue, otherValue) {
+        if (myValue === otherValue) {
+          ctrl.$setValidity('equal', true);
+          return myValue;
+        } else {
+          ctrl.$setValidity('equal', false);
+          return undefined;
+        }
+      }
+
+      scope.$watch(attrs.validateEquals, function(otherModelValue) {
+        ctrl.$setValidity('equal', ctrl.$viewValue === otherModelValue);
+        //TODO: something is missing here since editing other field to a valid value doesn't work properly
+      });
+
+      ctrl.$parsers.unshift(function(viewValue) {
+        return validateEqual(viewValue, scope.$eval(attrs.validateEquals));
+      });
+
+      ctrl.$formatters.unshift(function(modelValue) {
+        return validateEqual(modelValue, scope.$eval(attrs.validateEquals));
+      });
+    }
+  };
+});
