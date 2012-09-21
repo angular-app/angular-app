54f37e1d1c0e90a76075edea3bdc8c4967541492
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-09-21 17:05:58 +0200

    Add unique e-mail validation

--- src/modules/admin/admin-users.js
+++ src/modules/admin/admin-users.js
@@ -1,4 +1,4 @@
-angular.module('admin-users', ['services.crud'], ['$routeProvider', function($routeProvider){
+angular.module('admin-users', ['services.crud'], ['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/admin/users', {
     templateUrl:'admin/partials/users-list.tpl.html',
     controller:'AdminUsersCtrl',
@@ -41,5 +41,29 @@ angular.module('admin-users').controller('AdminUserEditCtrl', ['$scope', '$locat
   angular.extend($scope, new CRUDScopeMixIn('item', user, 'form', function () {
     $location.path('/admin/users');
   }));
+}]);
 
+angular.module('admin-users').directive('uniqueEmail', ["Users", function (Users) {
+  return {
+    require:'ngModel',
+    restrict:'A',
+    link:function (scope, el, attrs, ctrl) {
+
+      //using push() here to run it as the last parser, after we are sure that other validators were run
+      ctrl.$parsers.push(function (viewValue) {
+
+        ctrl.$setValidity('uniqueEmail', false);
+        if (viewValue) {
+          Users.query({email:viewValue}, function (users) {
+            if (users.length === 0) {
+              ctrl.$setValidity('uniqueEmail', true);
+            }
+          });
+          return viewValue;
+        } else {
+          return undefined;
+        }
+      });
+    }
+  };
 }]);
