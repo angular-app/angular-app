4d499f1a6828550f17c2912e89bbeb19a2c7e057
Pete Bacon Darwin <pete@bacondarwin.com>
2012-09-21 14:21:48 +0100

    Build file for the server!

---
+++ server/grunt.js
@@ -0,0 +1,36 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    lint: {
+      files: ['grunt.js', 'server.js', 'test/**/*.js']
+    },
+    test: {
+      files: ['test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint test'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true
+      },
+      globals: { require: false, __dirname: false, console: false }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint test');
+
+};
