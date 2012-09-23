b3e44e36d02e7f926b4cc824695b8646007b3245
Pete Bacon Darwin <pete@bacondarwin.com>
2012-09-23 22:33:03 +0100

    Tidied up a bit

--- src/services/mongo-resource/backend.js
+++ src/services/mongo-resource/backend.js
@@ -1,5 +1,5 @@
 angular.module('backendResource', ['mongoResourceImpl']).factory('mongoResource', ['mongoResourceImpl', function (mongoResourceImpl) {
   return function(collectionName) {
-    return mongoResourceImpl(collectionName, '/db/[[COLLECTION_NAME]]');
+    return mongoResourceImpl(collectionName, '/databases/[[COLLECTION_NAME]]');
   };
 }]);
