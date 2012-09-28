e7f3a52a4fcffd57ad24a43cd43e05eec9eb8a27
Pete Bacon Darwin <pete@bacondarwin.com>
2012-09-28 11:38:33 +0100

    Tidied up the config and added a travis hook

--- server.js
+++ server.js
@@ -1,50 +1,49 @@
 var express = require('express');
 var mongoProxy = require('./lib/mongo-proxy');
 var config = require('./config.js');
-
 var passport = require('passport');
 var MongoStrategy = require('./lib/mongo-strategy');
-
-var LISTEN_PORT = 3000;
-var distFolder = '../dist';
-var staticUrl = '/static';
 
 var app = express();
 
 // Serve up the favicon
-app.use(express.favicon(distFolder + '/favicon.ico'));
+app.use(express.favicon(config.server.distFolder + '/favicon.ico'));
 
 // First looks for a static file: index.html, css, images, etc.
-app.use(staticUrl, express['static'](distFolder));
-app.use(staticUrl, function(req, res, next) {
+app.use(config.server.staticUrl, express['static'](config.server.distFolder));
+app.use(config.server.staticUrl, function(req, res, next) {
   res.send(404); // If we get here then the request for a static file is invalid
 });
 
-app.use(express.logger());                      // Log requests to the console
-app.use(express.bodyParser());                  // Extract the data from the body of the request - this is needed by the LocalStrategy authenticate method
-app.use(express.cookieParser('angular-app'));   // Hash cookies with this secret
-app.use(express.cookieSession());               // Store the session in the (secret) cookie
-app.use(passport.initialize());                 // Initialize PassportJS
-app.use(passport.session());                    // Use Passport's session authentication strategy - this stores the logged in user in the session and will now run on any request
-passport.use(new MongoStrategy(config.dbUrl, config.apiKey, 'ascrum', 'users'));              // Add a Mongo strategy for handling the authentication
+app.use(express.logger());                                  // Log requests to the console
+app.use(express.bodyParser());                              // Extract the data from the body of the request - this is needed by the LocalStrategy authenticate method
+app.use(express.cookieParser(config.server.cookieSecret));  // Hash cookies with this secret
+app.use(express.cookieSession());                           // Store the session in the (secret) cookie
+app.use(passport.initialize());                             // Initialize PassportJS
+app.use(passport.session());                                // Use Passport's session authentication strategy - this stores the logged in user in the session and will now run on any request
+
+// Add a Mongo strategy for handling the authentication
+var mongoAuthStrategy = new MongoStrategy(config.mongo.dbUrl, config.mongo.apiKey, config.mongo.dbName, config.mongo.usersCollection);
+passport.use(mongoAuthStrategy);
 
 // Proxy database calls to the MongoDB
-app.use('/databases', mongoProxy(config.dbUrl, config.apiKey));
+app.use('/databases', mongoProxy(config.mongo.dbUrl, config.mongo.apiKey));
 
 // Login in to the app (using the mongo strategy)
-app.post('/login', passport.authenticate('mongo'));
+app.post('/login', passport.authenticate(mongoAuthStrategy.name));
 
 // Logout the current user
 app.post('/logout', function(req, res){ req.logOut(); });
 
 // This route deals enables HTML5Mode by forwarding missing files to the index.html
-// TODO: We should consider putting static files (images, etc) in a sub folder so that we get proper 404 errors for missing static files, rather than returning index.html!
 app.all('/*', function(req, res) {
   // Just send the index.html for other files to support HTML5Mode
-  res.sendfile('index.html', { root: distFolder });
+  res.sendfile('index.html', { root: config.server.distFolder });
 });
 
+// A standard error handler - it picks up any left over errors and returns a nicely formatted http 500 error
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
-app.listen(LISTEN_PORT);
-console.log('Angular App Server - listening on port: ' + LISTEN_PORT);
+// Start up the server on the port specified in the config
+app.listen(config.server.listenPort);
+console.log('Angular App Server - listening on port: ' + config.server.listenPort);
