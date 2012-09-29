525190322bd60f8249b4d849a64179190c64a4a0
Pete Bacon Darwin <pete@bacondarwin.com>
2012-09-29 21:52:48 +0100

    Authentication service with passing unit tests

--- src/common/services/authentication.js
+++ src/common/services/authentication.js
@@ -1,25 +1,10 @@
 // Based loosely around work by Witold Szczerba - https://github.com/witoldsz/angular-http-auth
 angular.module('services.authentication', []);
 
-angular.module('services.authentication').factory('AuthenticationFailedRetryQueue', ['$q', function($q) {
-  var retryQueue = [];
-  return {
-    pushItem: function(response) {
-      loginRequired = true;
-      var deferred = $q.defer();
-      retryQueue.push({ request: response.config, deferred: deferred});
-      return deferred.promise;
-    },
-    hasMore: function() {
-      return retryQueue.length > 0;
-    },
-    getNext: function() {
-      return retryQueue.shift();
-    }
-  };
-}]);
-
-angular.module('services.authentication').factory('AuthenticationService', ['$http', '$rootScope', 'AuthenticationFailedRetryQueue', function($http, queue) {
+// The AuthenticationService is the public API for this module.  Application developers should only need to use this service and not any of the others here.
+// The general idea is that you watch isLoginRequired for a change to true.  This happens when a http request returns 401 unauthorized. When this happens you need to show your login dialog box.
+// When the user has successfully logged in you call loginConfirmed to retry any queued requests.
+angular.module('services.authentication').factory('AuthenticationService', ['$http', 'AuthenticationRequestRetryQueue', function($http, queue) {
 
   function retryRequest (next) {
     $http(next.request).then(function(response) {
@@ -41,17 +26,35 @@ angular.module('services.authentication').factory('AuthenticationService', ['$ht
   };
 }]);
 
-angular.module('services.authentication').factory('AuthenticationIntercepter', ['AuthenticationFailedRetryQueue', function(queue) {
+angular.module('services.authentication').factory('AuthenticationRequestRetryQueue', ['$q', function($q) {
+  var retryQueue = [];
+  return {
+    pushRequest: function(request) {
+      loginRequired = true;
+      var deferred = $q.defer();
+      retryQueue.push({ request: request, deferred: deferred});
+      return deferred.promise;
+    },
+    hasMore: function() {
+      return retryQueue.length > 0;
+    },
+    getNext: function() {
+      return retryQueue.shift();
+    }
+  };
+}]);
+
+angular.module('services.authentication').factory('AuthenticationInterceptor', ['AuthenticationRequestRetryQueue', function(queue) {
   return function(promise) {
     return promise.then(null, function(response) {
       if (response.status === 401) {
-        return queue.pushItem(response);
+        return queue.pushRequest(response.config);
       }
-      return response;
+      return promise;
     });
   };
 }]);
 
 angular.module('services.authentication').config(['$httpProvider', function($httpProvider) {
-  $httpProvider.responseInterceptors.push('AuthenticationIntercepter');
+  $httpProvider.responseInterceptors.push('AuthenticationInterceptor');
 }]);
--- test/unit/common/services/authenticationSpec.js
+++ test/unit/common/services/authenticationSpec.js
@@ -1,29 +1,192 @@
-describe('Authentication Service', function () {
+describe('services.authentication', function() {
 
-  var $rootScope, callback, $httpBackend;
+  var $rootScope, $http, $httpBackend, success, error, status;
 
   beforeEach(module('services.authentication', 'ng'));
   beforeEach(inject(function($injector) {
     $rootScope = $injector.get('$rootScope');
     $httpBackend = $injector.get('$httpBackend');
-    $httpBackend.when('GET', '/').respond(401,'');
-    callback = jasmine.createSpy();
+    $http = $injector.get('$http');
+    success = jasmine.createSpy('success');
+    error = jasmine.createSpy('error');
   }));
 
   afterEach(function() {
     $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   });
 
-  describe('Retry Queue', function() {
-    it('does what it is supposed to do', function () {
-      inject(function($http) {
-        $http.get('/').then(function(result) {
-          throw new Error('Should not get here.');
-        }, function(error) {
-          expect(error).toBe(402);
+  var mockUpHttpResponse = function(status, data) {
+    $httpBackend.when('GET', '/').respond(status, data);
+    $http.get('/').then(success, error);
+  };
+
+  describe('Normal Responses', function() {
+    var testSuccessfulResponse = function(status, data) {
+        describe(status + ' HTTP Response', function() {
+          it('is not affected', function() {
+            mockUpHttpResponse(status, data);
+            $httpBackend.flush();
+            expect(success).toHaveBeenCalled();
+            expect(error).not.toHaveBeenCalled();
+          });
         });
-        $httpBackend.flush();
-      });
+      };
+
+    for (status = 200; status < 300; ++status) {
+      testSuccessfulResponse(status, 'Success: ' + status);
+    }
+
+    var testErrorResponse = function(status, data) {
+        describe(status + ' HTTP Response', function() {
+          it('is not affected', function() {
+            mockUpHttpResponse(status, data);
+            $httpBackend.flush();
+            expect(success).not.toHaveBeenCalled();
+            expect(error).toHaveBeenCalled();
+          });
+        });
+      };
+
+    testErrorResponse(400, 'Bad Request');
+    for (status = 402; status < 600; ++status) {
+      testErrorResponse(status, 'Error: ' + status);
+    }
+  });
+
+  describe('AuthenticationRequestRetryQueue', function() {
+    var queue;
+    beforeEach(inject(function($injector) {
+      queue = $injector.get('AuthenticationRequestRetryQueue');
+    }));
+
+    it('initially has no items to retry', function() {
+      expect(queue.hasMore).toBeDefined();
+      expect(queue.hasMore()).toBe(false);
+    });
+
+    it('pushing a request returns a promise', function() {
+      var promise = queue.pushRequest('request');
+      expect(promise.then).toBeDefined();
+    });
+
+    it('has more items once one has been pushed', function() {
+      queue.pushRequest('request');
+      expect(queue.hasMore()).toBe(true);
+    });
+
+    it('has no more items once all items have been got', function() {
+      queue.pushRequest('request');
+      var next = queue.getNext();
+      expect(queue.hasMore()).toBe(false);
+    });
+
+    it('stores a deferred object with the same promise as is returned from pushRequest', function() {
+      var promise = queue.pushRequest('request');
+      var next = queue.getNext();
+      expect(next.deferred.promise).toBe(promise);
+    });
+
+  });
+
+  describe('AuthenticationInterceptor', function() {
+    var queue, interceptor, promise, wrappedPromise;
+    beforeEach(inject(function($injector) {
+      queue = $injector.get('AuthenticationRequestRetryQueue');
+      interceptor = $injector.get('AuthenticationInterceptor');
+      wrappedPromise = {};
+      promise = {
+        then: jasmine.createSpy('then').andReturn(wrappedPromise)
+      };
+    }));
+
+    it('accepts and returns a promise', function() {
+      var newPromise = interceptor(promise);
+      expect(promise.then).toHaveBeenCalled();
+      expect(promise.then.mostRecentCall.args[0]).toBe(null);
+      expect(newPromise).toBe(wrappedPromise);
+    });
+
+    it('does not intercept non-401 error responses', function() {
+      var badmockUpHttpResponse = {
+        status: 400
+      };
+      interceptor(promise);
+      var errorHandler = promise.then.mostRecentCall.args[1];
+      expect(errorHandler(badmockUpHttpResponse)).toBe(promise);
+    });
+
+    it('intercepts 401 error responses and adds it to the retry queue', function() {
+      var notAuthResponse = {
+        status: 401
+      };
+      interceptor(promise);
+      var errorHandler = promise.then.mostRecentCall.args[1];
+      var newPromise = errorHandler(notAuthResponse);
+      expect(queue.getNext().deferred.promise).toBe(newPromise);
+    });
+  });
+
+  describe('AuthenticationService', function() {
+    var service;
+    beforeEach(inject(function($injector) {
+      service = $injector.get('AuthenticationService');
+    }));
+
+    it('should not require login before any responses have been received', function() {
+      expect(service.isLoginRequired()).toBe(false);
+      mockUpHttpResponse(401, 'Not Authorized');
+      expect(service.isLoginRequired()).toBe(false);
+      $httpBackend.flush();
+    });
+
+    it('should not require login after non-401 responses have been received', function() {
+      mockUpHttpResponse(400, 'Bad Request');
+      $httpBackend.flush();
+      mockUpHttpResponse(200, 'Success');
+      $httpBackend.flush();
+      expect(service.isLoginRequired()).toBe(false);
+    });
+
+    it('should require login after a 401 response has been received', function() {
+      mockUpHttpResponse(401, 'Not Authorized');
+      $httpBackend.flush();
+      expect(service.isLoginRequired()).toBe(true);
+    });
+
+    it('should require login after multiple 401 responses have been received', function() {
+      mockUpHttpResponse(401, 'Not Authorized');
+      $httpBackend.flush();
+      mockUpHttpResponse(200, 'Success');
+      mockUpHttpResponse(400, 'Bad Request');
+      mockUpHttpResponse(401, 'Not Authorized');
+      $httpBackend.flush();
+      expect(service.isLoginRequired()).toBe(true);
+    });
+
+    it('should not require login immediately after login confirmed', function() {
+      mockUpHttpResponse(401, 'Not Authorized');
+      $httpBackend.flush();
+      expect(service.isLoginRequired()).toBe(true);
+      service.loginConfirmed();
+      expect(service.isLoginRequired()).toBe(false);
+      $httpBackend.flush();
+    });
+
+    it('should not require login after login confirmed and requests are no longer failing with 401', function() {
+      var mock = $httpBackend.when('GET', '/');
+  
+      // Send a GET and respond with 401 No Authorized
+      mock.respond(401, "Not Authorized");
+      $http.get('/').then(success, error);
+      $httpBackend.flush();
+      expect(service.isLoginRequired()).toBe(true);
+
+      // Now clear the 401, confirm log-in and flush the retry responses
+      mock.respond(200, "Success");
+      service.loginConfirmed();
+      $httpBackend.flush();
+      expect(service.isLoginRequired()).toBe(false, 'Queue should now be empty');
     });
   });
 });
