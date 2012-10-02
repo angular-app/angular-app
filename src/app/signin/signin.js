3625c7e38a1104c08ad06db7ff3bd6c5f4ddf02a
Pete Bacon Darwin <pete@bacondarwin.com>
2012-10-02 18:53:18 +0100

    Added authService back into signin.js, since it is used in the template.

--- src/app/signin/signin.js
+++ src/app/signin/signin.js
@@ -1,7 +1,8 @@
-angular.module('signin', ['services.authentication', 'services.users']).controller('SignInCtrl', ['$scope', function($scope) {
+angular.module('signin', ['services.authentication', 'services.users']).controller('SignInCtrl', ['$scope', 'AuthenticationService', function($scope, AuthenticationService) {
 
   $scope.user = {};
   $scope.authError = false;
+  $scope.authService = AuthenticationService;
 
   $scope.clearForm = function() {
     $scope.user = {};
