b157f1717ada03dde9e5a6670d07711862d3f7b4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-10-08 19:18:49 +0200

    Improve breadcrumbs

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['login', 'dashboard', 'projects', 'admin', 'services.util', 'directives.crud', 'templates']);
+angular.module('app', ['login', 'dashboard', 'projects', 'admin', 'services.breadcrumbs', 'services.util', 'directives.crud', 'templates']);
 
 angular.module('app').constant('MONGOLAB_CONFIG', {
   baseUrl: 'http://localhost:3000/databases/',
@@ -12,34 +12,25 @@ angular.module('app').config(['$routeProvider', '$locationProvider', function ($
 
 angular.module('app').controller('AppCtrl', [function() {}]);
 
-angular.module('app').controller('HeaderCtrl', ['$scope', '$location', '$route', 'currentUser', 'HTTPRequestTracker', function ($scope, $location, $route, currentUser, HTTPRequestTracker) {
+angular.module('app').controller('HeaderCtrl', ['$scope', '$location', '$route', 'currentUser', 'breadcrumbs', 'HTTPRequestTracker', function ($scope, $location, $route, currentUser, breadcrumbs, HTTPRequestTracker) {
   $scope.location = $location;
   $scope.currentUser = currentUser;
-  
+  $scope.breadcrumbs = breadcrumbs;
+
   $scope.isNavbarActive = function (navBarPath) {
-    return navBarPath === $scope.pathElements[0];
-  };
-
-  $scope.$watch('location.path()', function (newValue) {
-    var pathElements = newValue.split('/');
-    pathElements.shift();
-    $scope.pathElements = pathElements;
-  });
-
-  $scope.breadcrumbPath = function (index) {
-    return '/' + ($scope.pathElements.slice(0, index + 1)).join('/');
+    return navBarPath === breadcrumbs.getFirst().name;
   };
 
   $scope.hasPendingRequests = function () {
     return HTTPRequestTracker.hasPendingRequests();
   };
 
+  $scope.$on('$routeChangeStart', function(event, next, current){
+    $scope.routeChangeError = undefined;
+  });
+
   $scope.$on('$routeChangeError', function(event, current, previous, rejection){
     //TODO: this is too MongoLab specific, itroduce error resolution service
     $scope.routeChangeError = 'Route change error: '+rejection.code;
-  });
-
-  $scope.$on('$routeChangeStart', function(event, next, current){
-    $scope.routeChangeError = undefined;
   });
 }]);
