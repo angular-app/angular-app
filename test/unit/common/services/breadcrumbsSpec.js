7824a39d67c9c7a8989dddc74839806b1686d03a
Pete Bacon Darwin <pete@bacondarwin.com>
2012-10-10 21:14:58 +0100

    Using spy to mock up location path function

--- test/unit/common/services/breadcrumbsSpec.js
+++ test/unit/common/services/breadcrumbsSpec.js
@@ -2,29 +2,33 @@ describe('breadcrumbs', function () {
 
   var LocationMock = function (initialPath) {
     var pathStr = initialPath || '';
-
     this.path = function (pathArg) {
       return pathArg ? pathStr = pathArg : pathStr;
     };
   };
 
-  beforeEach(function () {
-    angular.module('test', ['services.breadcrumbs']).value('$location', new LocationMock());
-  });
-  beforeEach(module('test'));
+  var $location, $rootScope, breadcrumbs;
 
-  it('should have sensible defaults before route navigation', inject(function (breadcrumbs) {
-    expect(breadcrumbs.getAll()).toEqual([]);
-    expect(breadcrumbs.getFirst()).toEqual({});
+  beforeEach(module('services.breadcrumbs'));
+  beforeEach(inject(function($injector) {
+    breadcrumbs = $injector.get('breadcrumbs');
+    $rootScope = $injector.get('$rootScope');
+    $location = $injector.get('$location');
+    spyOn($location, 'path').andCallFake(new LocationMock().path);
   }));
 
-  it('should not expose breadcrumbs before route change success', inject(function (breadcrumbs, $location) {
+  it('should have sensible defaults before route navigation', function() {
+    expect(breadcrumbs.getAll()).toEqual([]);
+    expect(breadcrumbs.getFirst()).toEqual({});
+  });
+
+  it('should not expose breadcrumbs before route change success', function () {
     $location.path('/some/path');
     expect(breadcrumbs.getAll()).toEqual([]);
     expect(breadcrumbs.getFirst()).toEqual({});
-  }));
+  });
 
-  it('should correctly parse $location() after route change success', inject(function (breadcrumbs, $location, $rootScope) {
+  it('should correctly parse $location() after route change success', function () {
     $location.path('/some/path');
     $rootScope.$broadcast('$routeChangeSuccess', {});
     expect(breadcrumbs.getAll()).toEqual([
@@ -32,6 +36,6 @@ describe('breadcrumbs', function () {
       { name:'path', path:'/some/path' }
     ]);
     expect(breadcrumbs.getFirst()).toEqual({name:'some', path:'/some'});
-  }));
+  });
 
 });
