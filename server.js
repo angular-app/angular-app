2f951a75820bc9346ad6a597d6cb7078ff6f1d92
Pete Bacon Darwin <pete@bacondarwin.com>
2012-10-15 12:34:23 +0100

    Added extra url end-points for checking authentication and authorization

--- server.js
+++ server.js
@@ -67,6 +67,16 @@ app.post('/logout', security.logout);
 // Retrieve the current user
 app.get('/current-user', security.sendCurrentUser);
 
+// Retrieve the current user only if they are authenticated
+app.get('/authenticated-user', function(req, res) {
+  security.authenticationRequired(req, res, function() { security.sendCurrentUser(req, res); });
+});
+
+// Retrieve the current user only if they are admin
+app.get('/admin-user', function(req, res) {
+  security.adminRequired(req, res, function() { security.sendCurrentUser(req, res); });
+});
+
 // This route deals enables HTML5Mode by forwarding missing files to the index.html
 app.all('/*', function(req, res) {
   // Just send the index.html for other files to support HTML5Mode
