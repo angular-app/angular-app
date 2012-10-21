b620b9f099fd654df9f9265b9c063a969f674c9d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-10-21 12:42:13 +0200

    Refactoring localized messages service based on PR review

--- src/common/services/localizedMessages.js
+++ src/common/services/localizedMessages.js
@@ -1,31 +1,17 @@
-angular.module('services.localizedMessages', []).factory('localizedMessages', ['$locale', '$interpolate', 'I18N.MESSAGES', 'I18N.DEFAULT_LOCALE', function ($locale, $interpolate, i18nmessages, i18ndefaultLocale) {
+angular.module('services.localizedMessages', []).factory('localizedMessages', ['$interpolate', 'I18N.MESSAGES', function ($interpolate, i18nmessages) {
 
-  var defaultLocale = i18ndefaultLocale;
-  var getForLocale = function(locale, msgKey, handleNotFound) {
-    return (i18nmessages[locale] || {})[msgKey];
+  var handleNotFound = function (msg, msgKey) {
+    return msg || '?' + msgKey + '?';
   };
 
-  var handleNotFound = function(msg, msgKey) {
-    return msg || '?'+msgKey+'?';
-  };
-
-  var msgService = {};
-   msgService.get = function(msgKey, interpolateParams) {
-     return msgService.getForLocale(msgKey, $locale.id, interpolateParams)
-   };
-
-    msgService.getForLocale = function(msgKey, locale, interpolateParams) {
-      var msgForLocale = getForLocale(locale || $locale.id, msgKey);
-      if (!msgForLocale && locale !== defaultLocale) {
-        msgForLocale =  getForLocale(defaultLocale, msgKey, true);
-      }
-
-      if (msgForLocale) {
-        return $interpolate(msgForLocale)(interpolateParams);
+  return {
+    get : function (msgKey, interpolateParams) {
+      var msg =  i18nmessages[msgKey];
+      if (msg) {
+        return $interpolate(msg)(interpolateParams);
       } else {
-        return handleNotFound(msgForLocale, msgKey);
+        return handleNotFound(msg, msgKey);
       }
-    };
-
-  return msgService;
+    }
+  };
 }]);
