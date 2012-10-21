6507541c3eba2dc0d99b383e17a54a530b5a375b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-10-21 11:38:00 +0200

    Further refactorings of the localization service

--- test/unit/common/services/notificationsSpec.js
+++ test/unit/common/services/notificationsSpec.js
@@ -10,30 +10,34 @@ describe('notifications', function () {
   describe('global notifications crud', function () {
 
     it('should allow to add, get and remove notifications', function () {
-      var not1 = notifications.pushStickyNotification('Watch out!', 'alert');
-      var not2 = notifications.pushStickyNotification('Just an info!', 'info');
+      var not1 = notifications.pushSticky('Watch out!', 'alert');
+      var not2 = notifications.pushSticky('Just an info!', 'info');
 
-      expect(notifications.current().length).toEqual(2);
-      expect(notifications.current()[0]).toBe(not1);
+      expect(notifications.getCurrent().length).toEqual(2);
+      expect(notifications.getCurrent()[0]).toBe(not1);
 
       notifications.remove(not2);
-      expect(notifications.current().length).toEqual(1);
-      expect(notifications.current()[0]).toBe(not1);
+      expect(notifications.getCurrent().length).toEqual(1);
+      expect(notifications.getCurrent()[0]).toBe(not1);
 
       notifications.removeAll();
-      expect(notifications.current().length).toEqual(0);
+      expect(notifications.getCurrent().length).toEqual(0);
+    });
+
+    it('removeal of a non-existing notification doesnt trigger errors', function () {
+      notifications.remove({});
     });
   });
 
   describe('notifications expiring after route change', function () {
 
     it('should remove notification after route change', function () {
-      var sticky = notifications.pushStickyNotification('Will stick around after route change');
-      var currentRoute = notifications.pushCurrentRouteNotification('Will go away after route change');
-      expect(notifications.current().length).toEqual(2);
+      var sticky = notifications.pushSticky('Will stick around after route change');
+      var currentRoute = notifications.pushForCurrentRoute('Will go away after route change');
+      expect(notifications.getCurrent().length).toEqual(2);
       $scope.$emit('$routeChangeSuccess');
-      expect(notifications.current().length).toEqual(1);
-      expect(notifications.current()[0]).toBe(sticky);
+      expect(notifications.getCurrent().length).toEqual(1);
+      expect(notifications.getCurrent()[0]).toBe(sticky);
     });
   });
 
@@ -41,13 +45,23 @@ describe('notifications', function () {
   describe('notifications showing on next route change and expiring on a subsequent one', function () {
 
     it('should advertise a notification after a route change and remove on the subsequent route change', function () {
-      notifications.pushStickyNotification('Will stick around after route change');
-      notifications.pushNextRouteNotification('Will not be there till after route change');
-      expect(notifications.current().length).toEqual(1);
+      notifications.pushSticky('Will stick around after route change');
+      notifications.pushForNextRoute('Will not be there till after route change');
+      expect(notifications.getCurrent().length).toEqual(1);
       $scope.$emit('$routeChangeSuccess');
-      expect(notifications.current().length).toEqual(2);
+      expect(notifications.getCurrent().length).toEqual(2);
       $scope.$emit('$routeChangeSuccess');
-      expect(notifications.current().length).toEqual(1);
+      expect(notifications.getCurrent().length).toEqual(1);
+    });
+  });
+
+  describe('removing a notification instance', function () {
+
+    it('should allow removal of notification instances', function () {
+      var sticky = notifications.pushSticky('Will be removed!');
+      expect(notifications.getCurrent().length).toEqual(1);
+     notifications.getCurrent()[0].$remove();
+     expect(notifications.getCurrent().length).toEqual(0);
     });
   });
 });
