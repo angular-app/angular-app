1cf0fb46e79479d703ddf158b5df6c2b1ce72de4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-10-21 20:28:25 +0200

    Integrating modal from angular-ui/bootstrap

--- src/common/directives/modal.js
+++ src/common/directives/modal.js
@@ -1,23 +1,75 @@
-angular.module('directives.modal', []).directive('modal', ['$parse', function($parse){
-  var directive = {
-    restrict: 'C',
-    link: function($scope, $element, $attrs, $controller){
-      var showFn = $parse($attrs.show);
+angular.module('directives.modal', []).directive('modal', ['$parse',function($parse) {
+  var backdropEl;
+  var body = angular.element(document.getElementsByTagName('body')[0]);
+  var defaultOpts = {
+    backdrop: true,
+    escape: true
+  };
+  return {
+    restrict: 'ECA',
+    link: function(scope, elm, attrs) {
+      var opts = angular.extend(defaultOpts, scope.$eval(attrs.uiOptions || attrs.bsOptions || attrs.options));
+      var shownExpr = attrs.modal || attrs.show;
+      var setClosed;
 
-      if ( !$element.modal ) {
-        throw new Error("Modal directive requires Twitter Bootstrap Modal library");
+      if (attrs.close) {
+        setClosed = function() {
+          scope.$apply(attrs.close);
+        };
+      } else {
+        setClosed = function() {
+          scope.$apply(function() {
+            $parse(shownExpr).assign(scope, false);
+          });
+        };
+      }
+      elm.addClass('modal');
+
+      if (opts.backdrop && !backdropEl) {
+        backdropEl = angular.element('<div class="modal-backdrop"></div>');
+        backdropEl.css('display','none');
+        body.append(backdropEl);
       }
 
-      $element.modal({ backdrop: $attrs.backdrop, keyboard: $attrs.keyboard, show: showFn($scope) });
+      function setShown(shown) {
+        scope.$apply(function() {
+          model.assign(scope, shown);
+        });
+      }
 
-      $scope.$watch($attrs.show, function(value) {
-        if ( value ) {
-          $element.modal('show');
+      function escapeClose(evt) {
+        if (evt.which === 27) { setClosed(); }
+      }
+      function clickClose() {
+        setClosed();
+      }
+
+      function close() {
+        if (opts.escape) { body.unbind('keyup', escapeClose); }
+        if (opts.backdrop) {
+          backdropEl.css('display', 'none').removeClass('in');
+          backdropEl.unbind('click', clickClose);
+        }
+        elm.css('display', 'none').removeClass('in');
+        body.removeClass('modal-open');
+      }
+      function open() {
+        if (opts.escape) { body.bind('keyup', escapeClose); }
+        if (opts.backdrop) {
+          backdropEl.css('display', 'block').addClass('in');
+          backdropEl.bind('click', clickClose);
+        }
+        elm.css('display', 'block').addClass('in');
+        body.addClass('modal-open');
+      }
+
+      scope.$watch(shownExpr, function(isShown, oldShown) {
+        if (isShown) {
+          open();
         } else {
-          $element.modal('hide');
+          close();
         }
       });
     }
   };
-  return directive;
 }]);
