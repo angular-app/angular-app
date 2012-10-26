39aa566306f31c9b79ab9977946e8af6e762d302
Pete Bacon Darwin <pete@bacondarwin.com>
2012-10-26 13:26:26 +0100

    Refactoring admin users edit screen

---
+++ src/app/admin/users/admin-users-edit.js
@@ -0,0 +1,79 @@
+angular.module('admin-users-edit',['services.crud'])
+
+.controller('UsersEditCtrl', ['$scope', '$location', 'crudEditMethods', 'user', function ($scope, $location, crudEditMethods, user) {
+  var successFn = function () {
+    $location.path('/admin/users');
+  };
+  var errorFn = function() {
+    $scope.updateError = true;
+  };
+
+  angular.extend($scope, crudEditMethods('item', user, 'form', successFn, errorFn));
+
+  $scope.password = user.password;
+
+  
+}])
+
+/**
+ * A validation directive to ensure that the model contains a unique email address
+ * @param  Users service to provide access to the server's user database
+  */
+.directive('uniqueEmail', ["Users", function (Users) {
+  return {
+    require:'ngModel',
+    restrict:'A',
+    link:function (scope, el, attrs, ctrl) {
+
+      //using push() here to run it as the last parser, after we are sure that other validators were run
+      ctrl.$parsers.push(function (viewValue) {
+
+        ctrl.$setValidity('uniqueEmail', false);
+        if (viewValue) {
+          Users.query({email:viewValue}, function (users) {
+            if (users.length === 0) {
+              ctrl.$setValidity('uniqueEmail', true);
+            }
+          });
+          return viewValue;
+        } else {
+          return undefined;
+        }
+      });
+    }
+  };
+}])
+
+/**
+ * A validation directive to ensure that this model has the same value as some other
+ */
+.directive('validateEquals', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+
+      function validateEqual(myValue, otherValue) {
+        if (myValue === otherValue) {
+          ctrl.$setValidity('equal', true);
+          return myValue;
+        } else {
+          ctrl.$setValidity('equal', false);
+          return undefined;
+        }
+      }
+
+      scope.$watch(attrs.validateEquals, function(otherModelValue) {
+        ctrl.$setValidity('equal', ctrl.$viewValue === otherModelValue);
+      });
+
+      ctrl.$parsers.push(function(viewValue) {
+        return validateEqual(viewValue, scope.$eval(attrs.validateEquals));
+      });
+
+      ctrl.$formatters.push(function(modelValue) {
+        return validateEqual(modelValue, scope.$eval(attrs.validateEquals));
+      });
+    }
+  };
+});
--- src/app/admin/users/admin-users.js
+++ src/app/admin/users/admin-users.js
@@ -1,4 +1,4 @@
-angular.module('admin-users', ['services.crud'], ['$routeProvider', function ($routeProvider) {
+angular.module('admin-users', ['admin-users-edit', 'services.crud'], ['$routeProvider', function ($routeProvider) {
 
   $routeProvider.when('/admin/users', {
     templateUrl:'admin/users/users-list.tpl.html',
@@ -43,70 +43,3 @@ angular.module('admin-users').controller('UsersListCtrl', ['$scope', 'crudListMe
 
   angular.extend($scope, crudListMethods('/admin/users'));
 }]);
-
-angular.module('admin-users').controller('UsersEditCtrl', ['$scope', '$location', 'crudEditMethods', 'user', function ($scope, $location, crudEditMethods, user) {
-
-  $scope.password = user.password;
-  angular.extend($scope, crudEditMethods('item', user, 'form', function () {
-    $location.path('/admin/users');
-  }, function() {
-    $scope.updateError = true;
-  }));
-}]);
-
-angular.module('admin-users').directive('uniqueEmail', ["Users", function (Users) {
-  return {
-    require:'ngModel',
-    restrict:'A',
-    link:function (scope, el, attrs, ctrl) {
-
-      //using push() here to run it as the last parser, after we are sure that other validators were run
-      ctrl.$parsers.push(function (viewValue) {
-
-        ctrl.$setValidity('uniqueEmail', false);
-        if (viewValue) {
-          Users.query({email:viewValue}, function (users) {
-            if (users.length === 0) {
-              ctrl.$setValidity('uniqueEmail', true);
-            }
-          });
-          return viewValue;
-        } else {
-          return undefined;
-        }
-      });
-    }
-  };
-}]);
-
-angular.module('admin-users').directive('validateEquals', function() {
-
-  return {
-    restrict: 'A',
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctrl) {
-
-      function validateEqual(myValue, otherValue) {
-        if (myValue === otherValue) {
-          ctrl.$setValidity('equal', true);
-          return myValue;
-        } else {
-          ctrl.$setValidity('equal', false);
-          return undefined;
-        }
-      }
-
-      scope.$watch(attrs.validateEquals, function(otherModelValue) {
-        ctrl.$setValidity('equal', ctrl.$viewValue === otherModelValue);
-      });
-
-      ctrl.$parsers.push(function(viewValue) {
-        return validateEqual(viewValue, scope.$eval(attrs.validateEquals));
-      });
-
-      ctrl.$formatters.push(function(modelValue) {
-        return validateEqual(modelValue, scope.$eval(attrs.validateEquals));
-      });
-    }
-  };
-});
