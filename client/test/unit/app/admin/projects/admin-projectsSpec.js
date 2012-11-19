54255cd7ccc77ed01e9716dc31fdb82c72da2007
Pete Bacon Darwin <pete@bacondarwin.com>
2012-11-19 12:04:10 +0000

    test(admin-projects): Add test for removeTeamMember

--- client/test/unit/app/admin/projects/admin-projectsSpec.js
+++ client/test/unit/app/admin/projects/admin-projectsSpec.js
@@ -22,35 +22,40 @@ describe('admin projects', function () {
   });
 
   describe('ProjectsEditCtrl', function () {
-    it('should call setup a scope correctly', function () {
-      var params = {
+    var params, ctrl;
+    beforeEach(function() {
+      params = {
         $scope: $scope,
         $location: jasmine.createSpyObj('$location', ['path']),
         users: [ { $id: function() { return 'X'; }} ],
         project: {}
       };
       params.users.filter = jasmine.createSpy('filter');
-
-      var ctrl = $controller('ProjectsEditCtrl', params);
-      
+      ctrl = $controller('ProjectsEditCtrl', params);
+    });
+    it('should call setup a scope objects correctly', function () {
       expect($scope.project).toBe(params.project);
       expect($scope.users).toBe(params.users);
       expect($scope.usersLookup).toEqual({ 'X': params.users[0] });
       expect($scope.project.teamMembers).toEqual([]);
-
+    });
+    it('should call $location in onSave', function() {
       $scope.onSave();
       expect(params.$location.path).toHaveBeenCalled();
-
+    });
+    it('should set updateError in onError', function() {
       $scope.onError();
       expect($scope.updateError).toBe(true);
-
+    });
+    it('should call filter when getting candidates', function() {
       $scope.productOwnerCandidates();
       expect(params.users.filter).toHaveBeenCalled();
       $scope.scrumMasterCandidates();
       expect(params.users.filter).toHaveBeenCalled();
       $scope.teamMemberCandidates();
       expect(params.users.filter).toHaveBeenCalled();
-
+    });
+    it('should push items into team members array in addTeamMember only if a team member is selected', function() {
       var pushTeamMember = spyOn($scope.project.teamMembers,'push');
       $scope.addTeamMember();
       expect(pushTeamMember).not.toHaveBeenCalled();
@@ -59,9 +64,17 @@ describe('admin projects', function () {
       $scope.addTeamMember();
       expect(pushTeamMember).toHaveBeenCalled();
       expect($scope.selTeamMember).toBeUndefined();
-
-      //TODO
+    });
+    it('should remove the specified team member if it is in the team members array', function() {
+      var indexOfTeamMembers = spyOn($scope.project.teamMembers,'indexOf').andReturn(-1);
       $scope.removeTeamMember();
+      expect(indexOfTeamMembers).toHaveBeenCalled();
+      
+      indexOfTeamMembers.andReturn(0);
+      spliceTeamMembers = spyOn($scope.project.teamMembers,'splice');
+      $scope.removeTeamMember();
+      expect(indexOfTeamMembers).toHaveBeenCalled();
+      expect(spliceTeamMembers).toHaveBeenCalled();
     });
   });
 });
