7d9272109f3210e5b5ada9367b25446f69ac82a2
Pete Bacon Darwin <pete@bacondarwin.com>
2012-11-25 21:00:45 +0000

    fix(server/config): Make distribution folder relative to the config file.
    Now you can run server.js from anywhere

--- server/config.js
+++ server/config.js
@@ -1,3 +1,5 @@
+path = require('path');
+
 module.exports = {
   mongo: {
     dbUrl: 'https://api.mongolab.com/api/1/databases',  // The base url of the MongoLab DB server
@@ -7,7 +9,7 @@ module.exports = {
   },
   server: {
     listenPort: 3000,                                   // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
-    distFolder: '../client/dist',                  // The folder that contains the application files (note that the files are in a different repository)
+    distFolder: path.resolve(__dirname, '../client/dist'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
     staticUrl: '/static',                               // The base url from which we serve static files (such as js, css and images)
     cookieSecret: 'angular-app'                         // The secret for encrypting the cookie
   }
