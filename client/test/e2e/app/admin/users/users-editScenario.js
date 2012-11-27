a26e8af78f852e6e3d29eb8ba09b2752f0d3ee6d
Pete Bacon Darwin <pete@bacondarwin.com>
2012-11-27 21:03:15 +0000

    test(users-edit): Fix e2e test.

--- client/test/e2e/app/admin/users/users-editScenario.js
+++ client/test/e2e/app/admin/users/users-editScenario.js
@@ -1,18 +1,13 @@
-myAppDev = angular.module('myAppDev', ['app', 'ngMockE2E']);
-
-myAppDev.run(function($httpBackend) {
-  // Mock up logged in admin user
-  $httpBackend.whenGET('/current-user').respond({user: { email: 'admin@abc.com', admin: true }});
-});
-
 describe('admin edit user', function() {
 
   beforeEach(function() {
     browser().navigateTo('/admin/users/new');
+    input('user.email').enter('admin@abc.com');
+    input('user.password').enter('changeme');
+    element('button.login').click();
   });
 
   it('enables the save button when the user info is filled in correctly', function() {
-    pause();
     expect(element('button.save:disabled').count()).toBe(1);
     input('user.email').enter('test@app.com');
     input('user.lastName').enter('Test');
