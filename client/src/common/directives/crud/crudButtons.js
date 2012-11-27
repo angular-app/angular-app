67bfbe0d8f63ca7eb1e3598073eabbba690d69e6
Pete Bacon Darwin <pete@bacondarwin.com>
2012-11-27 22:00:01 +0000

    refact(primary-buttons): Remove btn-primary css classes and use the primary-button directive instead.

--- client/src/common/directives/crud/crudButtons.js
+++ client/src/common/directives/crud/crudButtons.js
@@ -6,7 +6,7 @@ angular.module('directives.crud.buttons', [])
     replace:true,
     template:
       '<div>' +
-      '  <button class="btn-primary save" ng-disabled="!canSave()" ng-click="save()">Save</button>' +
+      '  <primary-button class="save" ng-disabled="!canSave()" ng-click="save()">Save</primary-button>' +
       '  <button class="btn-warning revert" ng-click="revertChanges()" ng-disabled="!canRevert()">Revert changes</button>'+
       '  <button class="btn-danger remove" ng-click="remove()" ng-show="canRemove()">Remove</button>'+
       '</div>'
