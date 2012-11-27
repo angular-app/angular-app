db0c2f2913d5621cc1058c881a6bcc20b4b5ff42
Pete Bacon Darwin <pete@bacondarwin.com>
2012-11-27 21:59:26 +0000

    feat(primary-button): Add new directive to create a BootStrap primary-button.

--- client/src/common/directives/button.js
+++ client/src/common/directives/button.js
@@ -1,8 +1,22 @@
-angular.module('directives.button', []).directive('button', function() {
+angular.module('directives.button', [])
+
+.directive('button', function() {
   return {
     restrict: 'E',
     link: function(scope, element) {
       element.addClass('btn');
+    }
+  };
+})
+
+.directive('primaryButton', function() {
+  return {
+    restrict: 'E',
+    template: '<button ng-transclude></button>',
+    transclude: true,
+    replace: true,
+    link: function(scope, element) {
+      element.addClass('btn-primary');
     }
   };
 });
--- client/test/unit/common/directives/buttonSpec.js
+++ client/test/unit/common/directives/buttonSpec.js
@@ -1,10 +1,32 @@
 describe('button directive', function () {
   beforeEach(module('directives.button'));
 
-  it('adds a "btn" class to button elements', function() {
+  it('adds a "btn" class to the button element', function() {
     inject(function($compile, $rootScope) {
       var element = $compile('<button></button>')($rootScope);
       expect(element.hasClass('btn')).toBe(true);
     });
+  });
+});
+
+describe('primaryButton directive', function () {
+  var element;
+  beforeEach(module('directives.button'));
+  beforeEach(function() {
+    inject(function($compile, $rootScope) {
+      element = $compile('<primary-button></primary-button>')($rootScope);
+    });
+  });
+
+  it('replaces the directive element with a button element', function() {
+    expect(element[0].localName).toBe('button');
+  });
+
+  it('adds a "btn-primary" class to the button element', function() {
+    expect(element.hasClass('btn')).toBe(true);
+  });
+
+  it('adds a "btn" class to button elements (because it is a button!)', function() {
+    expect(element.hasClass('btn')).toBe(true);
   });
 });
