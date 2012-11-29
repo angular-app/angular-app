472a04c1805cc731613ca13c2739f45b58c75ee6
Pete Bacon Darwin <pete@bacondarwin.com>
2012-11-29 21:10:34 +0000

    refact(validateEquals directive): Simplify the directive code.

--- client/src/app/admin/users/admin-users-edit.js
+++ client/src/app/admin/users/admin-users-edit.js
@@ -58,27 +58,17 @@ angular.module('admin-users-edit',['services.crud', 'services.i18nNotifications'
     restrict: 'A',
     require: 'ngModel',
     link: function(scope, elm, attrs, ctrl) {
-
-      function validateEqual(myValue, otherValue) {
-        if (myValue === otherValue) {
-          ctrl.$setValidity('equal', true);
-          return myValue;
-        } else {
-          ctrl.$setValidity('equal', false);
-          return undefined;
-        }
+      function validateEqual(myValue) {
+        var valid = (myValue === scope.$eval(attrs.validateEquals));
+        ctrl.$setValidity('equal', valid);
+        return valid ? myValue : undefined;
       }
 
-      scope.$watch(attrs.validateEquals, function(otherModelValue) {
-        ctrl.$setValidity('equal', ctrl.$viewValue === otherModelValue);
-      });
+      ctrl.$parsers.push(validateEqual);
+      ctrl.$formatters.push(validateEqual);
 
-      ctrl.$parsers.push(function(viewValue) {
-        return validateEqual(viewValue, scope.$eval(attrs.validateEquals));
-      });
-
-      ctrl.$formatters.push(function(modelValue) {
-        return validateEqual(modelValue, scope.$eval(attrs.validateEquals));
+      scope.$watch(attrs.validateEquals, function() {
+        ctrl.$setViewValue(ctrl.$viewValue);
       });
     }
   };
