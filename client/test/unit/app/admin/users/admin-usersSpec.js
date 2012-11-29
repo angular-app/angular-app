538f51f40671ba5b17b70272406f6a1a01af1756
Pete Bacon Darwin <pete@bacondarwin.com>
2012-11-29 21:10:00 +0000

    test(validateEquals directive): Test view changes

--- client/test/unit/app/admin/users/admin-usersSpec.js
+++ client/test/unit/app/admin/users/admin-usersSpec.js
@@ -54,49 +54,67 @@ describe('admin users', function () {
   });
 
   describe('validateEquals directive', function() {
-    var $scope, form;
-
-    function setTestValue(value) {
-      $scope.model.testValue = value;
-      $scope.$digest();
-    }
-    function setCompareTo(value) {
-      $scope.model.compareTo = value;
-      $scope.$digest();
-    }
+    var $scope, modelCtrl, modelValue;
 
     beforeEach(inject(function($compile, $rootScope) {
       $scope = $rootScope;
       var element = angular.element(
-        '<form name="form"><input name="testInput" ng-model="model.testValue" validate-equals="model.compareTo"></form>'
+        '<form name="testForm">' +
+          '<input name="testInput" ' +
+            'ng-model="model.testValue" ' +
+            'validate-equals="model.compareTo">' +
+        '</form>'
       );
-        $scope.model = {
-        testValue: '',
-        compareTo: ''
-      };
       $compile(element)($scope);
+      modelValue = $scope.model = {};
+      modelCtrl = $scope.testForm.testInput;
       $scope.$digest();
-      form = $scope.form;
     }));
 
-    describe('model validity', function() {
-      it('should be valid initially', function() {
-        expect(form.testInput.$valid).toBe(true);
-      });
+    it('should be valid initially', function() {
+      expect(modelCtrl.$valid).toBeTruthy();
+    });
+
+    describe('model value changes', function() {
       it('should be invalid if the model changes', function() {
-        setTestValue('different');
-        expect(form.testInput.$valid).toBe(false);
+        modelValue.testValue = 'different';
+        $scope.$digest();
+        expect(modelCtrl.$valid).toBeFalsy();
+        expect(modelCtrl.$viewValue).toBe(undefined);
       });
       it('should be invalid if the reference model changes', function() {
-        setCompareTo('different');
-        expect(form.testInput.$valid).toBe(false);
+        modelValue.compareTo = 'different';
+        $scope.$digest();
+        expect(modelCtrl.$valid).toBeFalsy();
+        expect(modelCtrl.$viewValue).toBe(undefined);
       });
-      it('should be valid if the model changes to be the same as the reference', function() {
-        setCompareTo('different');
-        expect(form.testInput.$valid).toBe(false);
+      it('should be valid if the modelValue changes to be the same as the reference', function() {
+        modelValue.compareTo = 'different';
+        $scope.$digest();
+        expect(modelCtrl.$valid).toBeFalsy();
 
-        setTestValue('different');
-        expect(form.testInput.$valid).toBe(true);
+        modelValue.testValue = 'different';
+        $scope.$digest();
+        expect(modelCtrl.$valid).toBeTruthy();
+        expect(modelCtrl.$viewValue).toBe('different');
+      });
+    });
+
+    describe('input value changes', function() {
+      it('should be invalid if the input value changes', function() {
+        modelCtrl.$setViewValue('different');
+        expect(modelCtrl.$valid).toBeFalsy();
+        expect(modelValue.testValue).toBe(undefined);
+      });
+
+      it('should be invalid if the input value changes to be the same as the reference', function() {
+        modelValue.compareTo = 'different';
+        $scope.$digest();
+        expect(modelCtrl.$valid).toBeFalsy();
+
+        modelCtrl.$setViewValue('different');
+        expect(modelCtrl.$viewValue).toBe('different');
+        expect(modelCtrl.$valid).toBeTruthy();
       });
     });
   });
