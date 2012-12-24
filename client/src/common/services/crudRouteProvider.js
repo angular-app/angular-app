fcb87e3e88c650910ad6cdf9e110fdaa79f417cd
Pete Bacon Darwin <pete@bacondarwin.com>
2012-12-24 15:48:52 +0000

    doc(crudRouteProvider): Added lots of comments to help people who come across this file!

--- client/src/common/services/crudRouteProvider.js
+++ client/src/common/services/crudRouteProvider.js
@@ -1,50 +1,105 @@
 (function() {
+
   function crudRouteProvider($routeProvider) {
 
-    this.$get = function () { return {}; }; //we are not interested in instances
+    // This $get noop is because at the moment in AngularJS "providers" must provide something
+    // via a $get method.
+    // When AngularJS has "provider helpers" then this will go away!
+    this.$get = angular.noop;
 
+    // Again, if AngularJS had "provider helpers" we might be able to return `routesFor()` as the
+    // crudRouteProvider itself.  Then we would have a much cleaner syntax and not have to do stuff
+    // like:
+    //
+    // ```
+    // myMod.config(function(crudRouteProvider) {
+    //   var routeProvider = crudRouteProvider.routesFor('MyBook', '/myApp');
+    // });
+    // ```
+    //
+    // but instead have something like:
+    //
+    //
+    // ```
+    // myMod.config(function(crudRouteProvider) {
+    //   var routeProvider = crudRouteProvider('MyBook', '/myApp');
+    // });
+    // ```
+    //
+    // In any case, the point is that this function is the key part of this "provider helper".
+    // We use it to create routes for CRUD operations.  We give it some basic information about
+    // the resource and the urls then it it returns our own special routeProvider.
     this.routesFor = function(resourceName, urlPrefix, routePrefix) {
       var baseUrl = urlPrefix + '/' + resourceName.toLowerCase();
       routePrefix = routePrefix || urlPrefix;
       var baseRoute = '/' + routePrefix + '/' + resourceName.toLowerCase();
 
-      var createRoute = function (operation, resolveFns) {
-        return {
-          templateUrl: baseUrl + '/' + resourceName.toLowerCase() +'-'+operation.toLowerCase()+'.tpl.html',
-          controller: resourceName + operation +'Ctrl',
-          resolve: resolveFns
-        };
+      // Create the templateUrl for a route to our resource that does the specified operation.
+      var templateUrl = function(operation) {
+        return baseUrl + '/' + resourceName.toLowerCase() +'-'+operation.toLowerCase()+'.tpl.html';
+      };
+      // Create the controller name for a route to our resource that does the specified operation.
+      var controllerName = function(operation) {
+        return resourceName + operation +'Ctrl';
       };
 
+      // This is the object that our `routesFor()` function returns.  It decorates `$routeProvider`,
+      // delegating the `when()` and `otherwise()` functions but also exposing some new functions for
+      // creating CRUD routes.  Specifically we have `whenList(), `whenNew()` and `whenEdit()`.
       var routeBuilder = {
+        // Create a route that will handle showing a list of items
         whenList: function(resolveFns) {
-          routeBuilder.when(baseRoute, createRoute('List', resolveFns));
+          routeBuilder.when(baseRoute, {
+            templateUrl: templateUrl('List'),
+            controller: controllerName('List'),
+            resolve: resolveFns
+          });
           return routeBuilder;
         },
+        // Create a route that will handle creating a new item
         whenNew: function(resolveFns) {
-          routeBuilder.when(baseRoute +'/new', createRoute('Edit', resolveFns));
+          routeBuilder.when(baseRoute +'/new', {
+            templateUrl: templateUrl('Edit'),
+            controller: controllerName('Edit'),
+            resolve: resolveFns
+          });
           return routeBuilder;
         },
+        // Create a route that will handle editing an existing item
         whenEdit: function(resolveFns) {
-          routeBuilder.when(baseRoute+'/:itemId', createRoute('Edit', resolveFns));
+          routeBuilder.when(baseRoute+'/:itemId', {
+            templateUrl: templateUrl('Edit'),
+            controller: controllerName('Edit'),
+            resolve: resolveFns
+          });
           return routeBuilder;
         },
+        // Pass-through to `$routeProvider.when()`
         when: function(path, route) {
           $routeProvider.when(path, route);
           return routeBuilder;
         },
+        // Pass-through to `$routeProvider.otherwise()`
         otherwise: function(params) {
           $routeProvider.otherwise(params);
           return routeBuilder;
         },
+        // Access to the core $routeProvider.
         $routeProvider: $routeProvider
       };
       return routeBuilder;
     };
-
   }
-  // Add our injection dependencies here since we cannot do it in module.provider()
+  // Currently, v1.0.3, AngularJS does not provide annotation style dependencies in providers so,
+  // we add our injection dependencies using the $inject form
   crudRouteProvider.$inject = ['$routeProvider'];
 
+  // Create our provider - it would be nice to be able to do something like this instead:
+  //
+  // ```
+  // angular.module('services.crudRouteProvider', [])
+  //   .configHelper('crudRouteProvider', ['$routeProvider, crudRouteProvider]);
+  // ```
+  // Then we could dispense with the $get, the $inject and the closure wrapper around all this.
   angular.module('services.crudRouteProvider', []).provider('crudRoute', crudRouteProvider);
 })();
