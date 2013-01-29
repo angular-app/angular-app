e4953ae70666aa27f1a16836b34a9b63fd73a237
Pete Bacon Darwin <pete@bacondarwin.com>
2013-01-29 15:06:44 +0000

    fix(initDB): Update in line with the change to config

--- server/initDB.js
+++ server/initDB.js
@@ -1,33 +1,38 @@
-var config = require('./config.js').mongo;
+var config = require('./config.js');
 var rest = require('restler');
-var baseUrl = config.dbUrl + '/' + config.dbName + '/collections/';
-var queryOptions = { apiKey: config.apiKey };
+var apiKey = config.mongo.apiKey;
+var baseUrl = config.mongo.dbUrl + '/databases/' + config.security.dbName + '/collections/';
 
 console.log('Configuration: \n', config);
 
 var checkDocument = function(collection, query, done) {
   var url = baseUrl + collection + '/';
-  var params = { apiKey:config.apiKey, q: JSON.stringify(query) };
+  console.log(url);
+  var params = { apiKey:apiKey, q: JSON.stringify(query) };
   var request = rest.get(url, { query: params });
-  request.on('error', function(err, response) { done(err, null); });
-  request.on('fail', function(err, response) { done(err, null); });
-  request.on('success', function(data) { done(null, data); });
+  request.on('error', function(err, response) { console.log('error', err, response); done(err, null); });
+  request.on('fail', function(err, response) {  console.log('fail', err, response); done(err, null); });
+  request.on('success', function(data) {  console.log('success', data); done(null, data); });
 };
 
 var createDocument = function(collection, document, done) {
-  var url = baseUrl + collection + '/?apiKey=' + config.apiKey;
+  var url = baseUrl + collection + '/?apiKey=' + apiKey;
+  console.log(url);
   var request = rest.postJson(url, document);
-  request.on('error', function(err, response) { done(err, null); });
-  request.on('fail', function(err, response) { done(err, null); });
-  request.on('success', function(data) { done(null, data); });
+  request.on('error', function(err, response) { console.log('error', err, response); done(err, null); });
+  request.on('fail', function(err, response) {  console.log('fail', err, response); done(err, null); });
+  request.on('success', function(data) {  console.log('success', data); done(null, data); });
 };
 
-console.log('Generating admin user...');
 var adminUser = { email: 'admin@abc.com', password: 'changeme', admin: true};
+console.log('Generating admin user...', adminUser);
 
-checkDocument(config.usersCollection, adminUser, function(err, data) {
+console.log('Checking it is not already created...');
+checkDocument(config.security.usersCollection, adminUser, function(err, data) {
   if ( !err && data.length === 0 ) {
-    createDocument(config.usersCollection, adminUser, function(err, data) {
+    console.log('Creating new admin user...', err, data);
+    createDocument(config.security.usersCollection, adminUser, function(err, data) {
+      console.log('Created new admin user...');
       console.log(err);
       console.log(data);
     });
