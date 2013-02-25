ec414d570cc59bb9c3dbf011e8c18e4b395de61a
Pete Bacon Darwin <pete@bacondarwin.com>
2013-02-25 09:34:15 +0000

    doc(authentication): Add better comments

--- client/src/common/services/authentication/authentication.js
+++ client/src/common/services/authentication/authentication.js
@@ -4,8 +4,8 @@ angular.module('services.authentication', ['services.authentication.currentUser'
 // The authentication is the public API for this module.  Application developers should only need to use this service and not any of the others here.
 .factory('authentication', ['$http', '$location', '$q', 'authenticationRetryQueue', 'currentUser', function($http, $location, $q, queue, currentUser) {
 
-  // TODO: We need a way to refresh the page to clear any data that has been loaded when the user logs out
-  //  a simple way would be to redirect to the root of the application but this feels a bit inflexible.
+  // We need a way to refresh the page to clear any data that has been loaded when the user logs out
+  //  a simple way is to redirect to the root of the application but this could be made more sophisticated
   function redirect(url) {
     url = url || '/';
     $location.path(url);
@@ -20,8 +20,11 @@ angular.module('services.authentication', ['services.authentication.currentUser'
   }
 
   var service = {
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // The following methods provide information you can bind to in the UI
+
     // Returns true if a login is required, perhaps because of a failed database request
-    // You can bind templates to the value returned to show a login form
     isLoginRequired: function() {
       return queue.hasMore();
     },
@@ -31,10 +34,14 @@ angular.module('services.authentication', ['services.authentication.currentUser'
       return queue.getReason();
     },
 
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // The following methods provide handlers for actions that could be triggered in the UI
+
     // Create a authentication queue item to trigger the login form to be shown
     showLogin: function() {
-      // Push a no-op onto the queue to create a manual login
-      queue.push({ retry: function() {}, cancel: function() {}, reason: 'user-request' });
+      // Push a dummy item onto the queue to create a manual login
+      queue.push({ retry: angular.noop, cancel: angular.noop, reason: 'user-request' });
     },
 
     // Attempt to authenticate a user by the given email and password
@@ -60,17 +67,11 @@ angular.module('services.authentication', ['services.authentication.currentUser'
       });
     },
 
-    // Ask the backend to see if a user is already authenticated - this may be from a previous session.
-    requestCurrentUser: function() {
-      if ( currentUser.isAuthenticated() ) {
-        return $q.when(currentUser);
-      } else {
-        return $http.get('/current-user').then(function(response) {
-          updateCurrentUser(response.data.user);
-          return currentUser;
-        });
-      }
-    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // The following methods support AngularJS routes.
+    // You can add them as resolves to routes to require authorization levels before allowing
+    // a route change to complete
 
     // Require that there is an authenticated user
     // (use this in a route resolve to prevent non-authenticated users from entering that route)
@@ -92,9 +93,21 @@ angular.module('services.authentication', ['services.authentication.currentUser'
         }
       });
       return promise;
+    },
+
+    // Ask the backend to see if a user is already authenticated - this may be from a previous session.
+    requestCurrentUser: function() {
+      if ( currentUser.isAuthenticated() ) {
+        return $q.when(currentUser);
+      } else {
+        return $http.get('/current-user').then(function(response) {
+          updateCurrentUser(response.data.user);
+          return currentUser;
+        });
+      }
     }
   };
-
+  
   // Get the current user when the service is instantiated
   // (in case they are still logged in from a previous session)
   service.requestCurrentUser();
