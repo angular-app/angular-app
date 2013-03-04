7e442a34ddac654815b9fc8f25839c13ae8be0c7
Pete Bacon Darwin <pete@bacondarwin.com>
2013-03-04 19:51:35 +0000

    chore(grunt): use npm version of html2js

--- client/gruntFile.js
+++ client/gruntFile.js
@@ -7,6 +7,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-testacular');
+  grunt.loadNpmTasks('grunt-html2js');
   grunt.loadTasks('build');
 
   // Default task.
