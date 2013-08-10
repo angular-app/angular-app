e93b78658d94b58cdbd97bcb570efe10d2b6d1d5
Pete Bacon Darwin <pete@bacondarwin.com>
2013-08-10 21:28:52 +0100

    style(productbacklog): make code less "spaghetti" :-)

--- client/src/app/projects/productbacklog/productbacklog.js
+++ client/src/app/projects/productbacklog/productbacklog.js
@@ -1,51 +1,68 @@
-angular.module('productbacklog', ['resources.productbacklog', 'services.crud']);
-angular.module('productbacklog').config(['crudRouteProvider', function(crudRouteProvider){
+angular.module('productbacklog', ['resources.productbacklog', 'services.crud'])
 
-  var projectId = ['$route', function($route) {
-    return $route.current.params.projectId;
-  }];
-
-  crudRouteProvider.routesFor('ProductBacklog', 'projects', 'projects/:projectId')
-
-  .whenList({
-    projectId: projectId,
-    backlog : ['$route', 'ProductBacklog', function($route, ProductBacklog){
-      return ProductBacklog.forProject($route.current.params.projectId);
-    }]
-  })
-
-  .whenNew({
-    projectId: projectId,
-    backlogItem : ['$route', 'ProductBacklog', function($route, ProductBacklog){
-      return new ProductBacklog({projectId:$route.current.params.projectId});
-    }]
-  })
-
-  .whenEdit({
-    projectId: projectId,
-    backlogItem : ['$route', 'ProductBacklog', function($route, ProductBacklog){
-      return ProductBacklog.getById($route.current.params.itemId);
-    }]
-  });
-}]);
-
-angular.module('productbacklog').controller('ProductBacklogListCtrl', ['$scope', 'crudListMethods', 'projectId', 'backlog', function($scope, crudListMethods, projectId, backlog){
-  $scope.backlog = backlog;
-
-  angular.extend($scope, crudListMethods('/projects/'+projectId+'/productbacklog'));
-}]);
-
-angular.module('productbacklog').controller('ProductBacklogEditCtrl', ['$scope', '$location', 'projectId', 'backlogItem', function($scope, $location, projectId, backlogItem){
-
-  $scope.backlogItem = backlogItem;
-
-  $scope.onSave = function () {
-    //TODO: missing message
-    $location.path('/projects/'+projectId+'/productbacklog');
-  };
-
-  $scope.onError = function () {
-    //TODO: missing message
-    $scope.updateError = true;
-  };
-}]);
+  .config(['crudRouteProvider', function(crudRouteProvider){
+  
+  
+    // projectId is a helper method wrapped with DI annotation that will be used in
+    // route resolves in this file.
+    var projectId = ['$route', function($route) {
+      return $route.current.params.projectId;
+    }];
+  
+  
+    // Create the CRUD routes for editing the product backlog
+    crudRouteProvider.routesFor('ProductBacklog', 'projects', 'projects/:projectId')
+      // How to handle the "list product backlog items" route
+      .whenList({
+        projectId: projectId,
+        backlog : ['$route', 'ProductBacklog', function($route, ProductBacklog){
+          return ProductBacklog.forProject($route.current.params.projectId);
+        }]
+      })
+      
+      // How to handle the "create a new product backlog item" route
+      .whenNew({
+        projectId: projectId,
+        backlogItem : ['$route', 'ProductBacklog', function($route, ProductBacklog){
+          return new ProductBacklog({projectId:$route.current.params.projectId});
+        }]
+      })
+    
+      // How to handle the "edit a product backlog item" route
+      .whenEdit({
+        projectId: projectId,
+        backlogItem : ['$route', 'ProductBacklog', function($route, ProductBacklog){
+          return ProductBacklog.getById($route.current.params.itemId);
+        }]
+      });
+  }])
+  
+  
+  
+  // The controller for listing product backlog items
+  .controller('ProductBacklogListCtrl', ['$scope', 'crudListMethods', 'projectId', 'backlog', function($scope, crudListMethods, projectId, backlog){
+  
+    $scope.backlog = backlog;
+    
+    angular.extend($scope, crudListMethods('/projects/'+projectId+'/productbacklog'));
+  
+  }])
+  
+  
+  
+  // The controller for editing a product backlog item
+  .controller('ProductBacklogEditCtrl', ['$scope', '$location', 'projectId', 'backlogItem', function($scope, $location, projectId, backlogItem){
+  
+    $scope.backlogItem = backlogItem;
+  
+    $scope.onSave = function () {
+      //TODO: missing message
+      $location.path('/projects/'+projectId+'/productbacklog');
+    };
+  
+    $scope.onError = function () {
+      //TODO: missing message
+      $scope.updateError = true;
+    };
+  
+  }]);
