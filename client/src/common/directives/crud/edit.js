fe339b122e9347b09e146801dd88078a5905bbe1
Cristian Ronzio <indixolutions@gmail.com>
2014-01-09 18:14:49 +0100

    Fix crudEdit directive by referencing to $parent.scope where needed

--- client/src/common/directives/crud/edit.js
+++ client/src/common/directives/crud/edit.js
@@ -40,7 +40,7 @@ angular.module('directives.crud.edit', [])
       };
       // Set up callbacks with fallback
       // onSave attribute -> onSave scope -> noop
-      var userOnSave = attrs.onSave ? makeFn('onSave') : ( $parent.scope.onSave || angular.noop );
+      var userOnSave = attrs.onSave ? makeFn('onSave') : ( scope.onSave || angular.noop );
       var onSave = function(result, status, headers, config) {
         // Reset the original to help with reverting and pristine checks
         original = result;
